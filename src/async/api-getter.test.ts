b9c635249e29b8fd216bb389a944d4e27becf989
Luke-Draper <lukepdraper@gmail.com>
2018-11-01 22:11:34 -0500

    test(axios tests): Add functional mock axios testing

--- src/async/api-getter.test.ts
+++ src/async/api-getter.test.ts
@@ -1,13 +1,22 @@
 import APIGetter from './api-getter'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
 
 /**
  * APIGetter test
  */
 describe('APIGetter test', () => {
 	test('APIGetter getRequest', () => {
-		expect.assertions(1)
-		return APIGetter.getRequest().then(response =>
-			expect(response).not.toBe(undefined)
-		)
+		// Setup
+		const mock = new MockAdapter(axios)
+		mock
+			.onGet('https://api.guildwars2.com/v2/')
+			.reply(200, { location: 'api_root_endpoint' })
+		// Tests
+		expect.assertions(2)
+		return APIGetter.getRequest().then(response => {
+			expect(response).not.toEqual(undefined)
+			expect(response.data).toEqual({ location: 'api_root_endpoint' })
+		})
 	})
 })
--- src/async/api-getter.ts
+++ src/async/api-getter.ts
@@ -1,24 +1,19 @@
 import axios from 'axios'
-import { stringify } from 'querystring'
 
 // ?access_token=<API key>
 
 export default class APIGetter {
-	static defaultRoot: string = 'https://api.guildwars2.com/v2'
+	static defaultRoot: string = 'https://api.guildwars2.com/v2/'
 	static getRequest(url: string = APIGetter.defaultRoot, params: object = {}) {
-		console.log(
-			`Sending GET Request to :${url} with Parameters:${JSON.stringify(params)}`
-		)
+		// console.log( `Sending GET Request to :${url} with Parameters:${JSON.stringify(params)}` )
 		return axios
 			.get(url, {
 				params: params
 			})
 			.then(function(response) {
-				console.log(response.data)
 				return response
 			})
 			.catch(function(error) {
-				console.log(error)
 				// return Promise.reject(error) // Use this to ignore the error
 				throw error
 			})
